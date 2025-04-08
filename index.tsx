8fc410f3bfde1cf12d151c4fea615c3a7f50bc00
qouesm <qouesm@tuta.io>
2025-04-08 16:34:42 -0400

    Format index.tsx. (mostly 4spc -> 2spc)

--- index.tsx
+++ index.tsx
@@ -6,60 +6,59 @@ import { findByPropsLazy } from "@webpack";
 import { ApplicationAssetUtils, FluxDispatcher, Forms } from "@webpack/common";
 
 interface ActivityAssets {
-    large_image?: string;
-    large_text?: string;
-    small_image?: string;
-    small_text?: string;
+  large_image?: string;
+  large_text?: string;
+  small_image?: string;
+  small_text?: string;
 }
 
 interface ActivityButton {
-    label: string;
-    url: string;
+  label: string;
+  url: string;
 }
 
 interface Activity {
-    state: string;
-    details?: string;
-    timestamps?: {
-        start?: number;
-    };
-    assets?: ActivityAssets;
-    buttons?: Array<string>;
-    name: string;
-    application_id: string;
-    metadata?: {
-        button_urls?: Array<string>;
-    };
-    type: number;
-    flags: number;
+  state: string;
+  details?: string;
+  timestamps?: {
+    start?: number;
+  };
+  assets?: ActivityAssets;
+  buttons?: Array<string>;
+  name: string;
+  application_id: string;
+  metadata?: {
+    button_urls?: Array<string>;
+  };
+  type: number;
+  flags: number;
 }
 
 interface TrackData {
-    name: string;
-    album: string;
-    artist: string;
-    url: string;
-    imageUrl?: string;
+  name: string;
+  album: string;
+  artist: string;
+  url: string;
+  imageUrl?: string;
 }
 
 // only relevant enum values
 const enum ActivityType {
-    PLAYING = 0,
-    LISTENING = 2,
+  PLAYING = 0,
+  LISTENING = 2,
 }
 
 const enum ActivityFlag {
-    INSTANCE = 1 << 0,
+  INSTANCE = 1 << 0,
 }
 
 const enum NameFormat {
-    StatusName = "status-name",
-    ArtistFirst = "artist-first",
-    SongFirst = "song-first",
-    ArtistOnly = "artist",
-    SongOnly = "song"
+  StatusName = "status-name",
+  ArtistFirst = "artist-first",
+  SongFirst = "song-first",
+  ArtistOnly = "artist",
+  SongOnly = "song",
 }
-
 
 const applicationId = "1090155131007406132";
 const placeholderId = "2a96cbd8b46e442fc41c2b86b821562f";
@@ -69,271 +68,288 @@ const logger = new Logger("ListenBrainzRPC");
 const presenceStore = findByPropsLazy("getLocalPresence");
 
 async function getApplicationAsset(key: string): Promise<string> {
-    return (await ApplicationAssetUtils.fetchAssetIds(applicationId, [key]))[0];
+  return (await ApplicationAssetUtils.fetchAssetIds(applicationId, [key]))[0];
 }
 
 function setActivity(activity: Activity | null) {
-    FluxDispatcher.dispatch({
-        type: "LOCAL_ACTIVITY_UPDATE",
-        activity,
-        socketId: "ListenBrainz",
-    });
+  FluxDispatcher.dispatch({
+    type: "LOCAL_ACTIVITY_UPDATE",
+    activity,
+    socketId: "ListenBrainz",
+  });
 }
 
 const settings = definePluginSettings({
-    username: {
-        description: "ListenBrainz username",
-        type: OptionType.STRING,
-    },
-    mbContact: {
-        description: "ListenBrainz contact",
-        type: OptionType.STRING,
-    },
-    shareUsername: {
-        description: "show link to ListenBrainz profile",
-        type: OptionType.BOOLEAN,
-        default: false,
-    },
-    hideWithSpotify: {
-        description: "hide ListenBrainz presence if Spotify is running",
-        type: OptionType.BOOLEAN,
+  username: {
+    description: "ListenBrainz username",
+    type: OptionType.STRING,
+  },
+  mbContact: {
+    description: "ListenBrainz contact",
+    type: OptionType.STRING,
+  },
+  shareUsername: {
+    description: "show link to ListenBrainz profile",
+    type: OptionType.BOOLEAN,
+    default: false,
+  },
+  hideWithSpotify: {
+    description: "hide ListenBrainz presence if Spotify is running",
+    type: OptionType.BOOLEAN,
+    default: true,
+  },
+  statusName: {
+    description: "custom status text",
+    type: OptionType.STRING,
+    default: "some music",
+  },
+  nameFormat: {
+    description: "Show name of song and artist in status name",
+    type: OptionType.SELECT,
+    options: [
+      {
+        label: "Use custom status name",
+        value: NameFormat.StatusName,
         default: true,
-    },
-    statusName: {
-        description: "custom status text",
-        type: OptionType.STRING,
-        default: "some music",
-    },
-    nameFormat: {
-        description: "Show name of song and artist in status name",
-        type: OptionType.SELECT,
-        options: [
-            {
-                label: "Use custom status name",
-                value: NameFormat.StatusName,
-                default: true
-            },
-            {
-                label: "Use format 'artist - song'",
-                value: NameFormat.ArtistFirst
-            },
-            {
-                label: "Use format 'song - artist'",
-                value: NameFormat.SongFirst
-            },
-            {
-                label: "Use artist name only",
-                value: NameFormat.ArtistOnly
-            },
-            {
-                label: "Use song name only",
-                value: NameFormat.SongOnly
-            }
-        ],
-    },
-    useListeningStatus: {
-        description: 'show "Listening to" status instead of "Playing"',
-        type: OptionType.BOOLEAN,
-        default: false,
-    },
-    missingArt: {
-        description: "When album or album art is missing",
-        type: OptionType.SELECT,
-        options: [
-            {
-                label: "Use large ListenBrainz logo",
-                value: "listenbrainzLogo",
-                default: true
-            },
-            {
-                label: "Use generic placeholder",
-                value: "placeholder"
-            }
-        ],
-    },
-    useLogo: {
-        description: "Show ListenBrainz logo on album art",
-        type: OptionType.BOOLEAN,
+      },
+      {
+        label: "Use format 'artist - song'",
+        value: NameFormat.ArtistFirst,
+      },
+      {
+        label: "Use format 'song - artist'",
+        value: NameFormat.SongFirst,
+      },
+      {
+        label: "Use artist name only",
+        value: NameFormat.ArtistOnly,
+      },
+      {
+        label: "Use song name only",
+        value: NameFormat.SongOnly,
+      },
+    ],
+  },
+  useListeningStatus: {
+    description: 'show "Listening to" status instead of "Playing"',
+    type: OptionType.BOOLEAN,
+    default: false,
+  },
+  missingArt: {
+    description: "When album or album art is missing",
+    type: OptionType.SELECT,
+    options: [
+      {
+        label: "Use large ListenBrainz logo",
+        value: "listenbrainzLogo",
         default: true,
-    }
+      },
+      {
+        label: "Use generic placeholder",
+        value: "placeholder",
+      },
+    ],
+  },
+  useLogo: {
+    description: "Show ListenBrainz logo on album art",
+    type: OptionType.BOOLEAN,
+    default: true,
+  },
 });
 
 export default definePlugin({
-    name: "ListenBrainzRPC",
-    description: "Little plugin for ListenBrainz rich presence",
-    authors: [
-        {
-            id: 130388483494641664n,
-            name: "qouesm",
-        },
-    ],
-
-    settingsAboutComponent: () => (
-        <>
-            <Forms.FormTitle tag="h3">About MusicBrainz API</Forms.FormTitle>
-            <Forms.FormText>
-                The MusicBrainz API does not require an API key, but it does require
-                a <Link href="https://musicbrainz.org/doc/MusicBrainz_API/Rate_Limiting#Provide_meaningful_User-Agent_strings"> meaningful user-agent string</Link> .
-                For most, an email address should suffice.
-            </Forms.FormText>
-        </>
-    ),
-
-    settings,
-
-    start() {
-        this.updatePresence();
-        this.updateInterval = setInterval(() => { this.updatePresence(); }, 16000);
+  name: "ListenBrainzRPC",
+  description: "Little plugin for ListenBrainz rich presence",
+  authors: [
+    {
+      id: 130388483494641664n,
+      name: "qouesm",
     },
+  ],
 
-    stop() {
-        clearInterval(this.updateInterval);
-    },
+  settingsAboutComponent: () => (
+    <>
+      <Forms.FormTitle tag="h3">About MusicBrainz API</Forms.FormTitle>
+      <Forms.FormText>
+        The MusicBrainz API does not require an API key, but it does require a{" "}
+        <Link href="https://musicbrainz.org/doc/MusicBrainz_API/Rate_Limiting#Provide_meaningful_User-Agent_strings">
+          {" "}
+          meaningful user-agent string
+        </Link>{" "}
+        . For most, an email address should suffice.
+      </Forms.FormText>
+    </>
+  ),
 
-    async fetchTrackData(): Promise<TrackData | null> {
-        if (!settings.store.username)
-            return null;
+  settings,
 
-        try {
-            const lbRes = await fetch(`https://api.listenbrainz.org/1/user/${settings.store.username}/playing-now`);
-            if (!lbRes.ok) throw `${lbRes.status} ${lbRes.statusText}`;
+  start() {
+    this.updatePresence();
+    this.updateInterval = setInterval(() => {
+      this.updatePresence();
+    }, 16000);
+  },
 
-            const lbJson = await lbRes.json();
-            if (lbJson.error) {
-                logger.error("Error from ListenBrainz API", `${lbJson.error}: ${lbJson.message}`);
-                return null;
-            }
+  stop() {
+    clearInterval(this.updateInterval);
+  },
 
-            const listen = lbJson.payload?.listens?.[0];
-            if (!listen?.playing_now) return null;
+  async fetchTrackData(): Promise<TrackData | null> {
+    if (!settings.store.username) return null;
 
-            const trackMetadata = listen.track_metadata;
-            const albumName = trackMetadata.release_name || "Unknown";
+    try {
+      const lbRes = await fetch(
+        `https://api.listenbrainz.org/1/user/${settings.store.username}/playing-now`,
+      );
+      if (!lbRes.ok) throw `${lbRes.status} ${lbRes.statusText}`;
 
-            const artistName = trackMetadata.artist_name || "Unknown";
+      const lbJson = await lbRes.json();
+      if (lbJson.error) {
+        logger.error(
+          "Error from ListenBrainz API",
+          `${lbJson.error}: ${lbJson.message}`,
+        );
+        return null;
+      }
 
-            const mbRes = await fetch(
-                `https://musicbrainz.org/ws/2/release/?query=release:
-                ${encodeURIComponent(albumName)}%20AND%20artist:${encodeURIComponent(artistName)}&fmt=json`
-            );
+      const listen = lbJson.payload?.listens?.[0];
+      if (!listen?.playing_now) return null;
 
-            if (!mbRes.ok) throw `${mbRes.status} ${mbRes.statusText}`;
+      const trackMetadata = listen.track_metadata;
+      const albumName = trackMetadata.release_name || "Unknown";
 
-            const mbJson = await mbRes.json();
-            const releases = mbJson.releases || [];
+      const artistName = trackMetadata.artist_name || "Unknown";
 
-            let releaseGroup = releases[0]['release-group'].id;
+      const mbRes = await fetch(
+        `https://musicbrainz.org/ws/2/release/?query=
+          release:${encodeURIComponent(albumName)}%20AND%20
+          artist:${encodeURIComponent(artistName)}&fmt=json`,
+      );
 
-            const caaRes = await fetch(`https://coverartarchive.org/release-group/${releaseGroup}`)
-            if (!caaRes.ok) throw `${caaRes.status} ${caaRes.statusText}`;
-            const caaJson = await caaRes.json();
+      if (!mbRes.ok) throw `${mbRes.status} ${mbRes.statusText}`;
 
-            const url: string = caaJson.release;
+      const mbJson = await mbRes.json();
+      const releases = mbJson.releases || [];
 
-            const images: string = caaJson["images"]
-            let imageUrl: string = "";
-            for (const image of images) {
-                imageUrl = image["thumbnails"].large || "";
-                if (!imageUrl)
-                    continue;
-                break;
-            }
+      let releaseGroup = releases[0]["release-group"].id;
 
-            return {
-                name: trackMetadata.track_name || "Unknown",
-                album: albumName,
-                artist: artistName,
-                url: url,
-                imageUrl: imageUrl,
-            };
-        } catch (e) {
-            logger.error("Failed to query ListenBrainz API", e);
-            // will clear the rich presence if API fails
-            return null;
-        }
-    },
+      const caaRes = await fetch(
+        `https://coverartarchive.org/release-group/${releaseGroup}`,
+      );
+      if (!caaRes.ok) throw `${caaRes.status} ${caaRes.statusText}`;
+      const caaJson = await caaRes.json();
 
-    async updatePresence() {
-        setActivity(await this.getActivity());
-    },
+      const url: string = caaJson.release;
 
-    getLargeImage(track: TrackData): string | undefined {
-        if (track.imageUrl && !track.imageUrl.includes(placeholderId))
-            return track.imageUrl;
+      const images: string = caaJson["images"];
+      let imageUrl: string = "";
+      for (const image of images) {
+        imageUrl = image["thumbnails"].large || "";
+        if (!imageUrl) continue;
+        break;
+      }
 
-        if (settings.store.missingArt === "placeholder")
-            return "placeholder";
-    },
-
-    async getActivity(): Promise<Activity | null> {
-        if (settings.store.hideWithSpotify) {
-            for (const activity of presenceStore.getActivities()) {
-                if (activity.type === ActivityType.LISTENING && activity.application_id !== applicationId) {
-                    // there is already music status because of Spotify or richerCider (probably more)
-                    return null;
-                }
-            }
-        }
-
-        const trackData = await this.fetchTrackData();
-        if (!trackData) return null;
-
-        const largeImage = this.getLargeImage(trackData);
-        const assets: ActivityAssets = largeImage ?
-            {
-                large_image: await getApplicationAsset(largeImage),
-                large_text: trackData.album || undefined,
-                small_image: settings.store.useLogo ? await getApplicationAsset("listenbrainz") : undefined,
-                small_text: "ListenBrainz",
-            } : {
-                large_image: await getApplicationAsset("listenbrainz"),
-                large_text: trackData.album || undefined,
-            };
-
-        const buttons: ActivityButton[] = [
-            {
-                label: "View Song",
-                url: trackData.url,
-            },
-        ];
-
-        if (settings.store.shareUsername)
-            buttons.push({
-                label: "ListenBrainz Profile",
-                url: `https://www.listenbrainz.org/user/${settings.store.username}`,
-            });
-
-        const statusName = (() => {
-            switch (settings.store.nameFormat) {
-                case NameFormat.ArtistFirst:
-                    return trackData.artist + " - " + trackData.name;
-                case NameFormat.SongFirst:
-                    return trackData.name + " - " + trackData.artist;
-                case NameFormat.ArtistOnly:
-                    return trackData.artist;
-                case NameFormat.SongOnly:
-                    return trackData.name;
-                default:
-                    return settings.store.statusName;
-            }
-        })();
-
-
-        return {
-            application_id: applicationId,
-            name: statusName,
-
-            details: trackData.name,
-            state: trackData.artist,
-            assets,
-
-            buttons: buttons.map(v => v.label),
-            metadata: {
-                button_urls: buttons.map(v => v.url),
-            },
-
-            type: settings.store.useListeningStatus ? ActivityType.LISTENING : ActivityType.PLAYING,
-            flags: ActivityFlag.INSTANCE,
-        };
+      return {
+        name: trackMetadata.track_name || "Unknown",
+        album: albumName,
+        artist: artistName,
+        url: url,
+        imageUrl: imageUrl,
+      };
+    } catch (e) {
+      logger.error("Failed to query ListenBrainz API", e);
+      // will clear the rich presence if API fails
+      return null;
     }
+  },
+
+  async updatePresence() {
+    setActivity(await this.getActivity());
+  },
+
+  getLargeImage(track: TrackData): string | undefined {
+    if (track.imageUrl && !track.imageUrl.includes(placeholderId))
+      return track.imageUrl;
+
+    if (settings.store.missingArt === "placeholder") return "placeholder";
+  },
+
+  async getActivity(): Promise<Activity | null> {
+    if (settings.store.hideWithSpotify) {
+      for (const activity of presenceStore.getActivities()) {
+        if (
+          activity.type === ActivityType.LISTENING &&
+          activity.application_id !== applicationId
+        ) {
+          // there is already music status because of Spotify or richerCider (probably more)
+          return null;
+        }
+      }
+    }
+
+    const trackData = await this.fetchTrackData();
+    if (!trackData) return null;
+
+    const largeImage = this.getLargeImage(trackData);
+    const assets: ActivityAssets = largeImage
+      ? {
+          large_image: await getApplicationAsset(largeImage),
+          large_text: trackData.album || undefined,
+          small_image: settings.store.useLogo
+            ? await getApplicationAsset("listenbrainz")
+            : undefined,
+          small_text: "ListenBrainz",
+        }
+      : {
+          large_image: await getApplicationAsset("listenbrainz"),
+          large_text: trackData.album || undefined,
+        };
+
+    const buttons: ActivityButton[] = [
+      {
+        label: "View Song",
+        url: trackData.url,
+      },
+    ];
+
+    if (settings.store.shareUsername)
+      buttons.push({
+        label: "ListenBrainz Profile",
+        url: `https://www.listenbrainz.org/user/${settings.store.username}`,
+      });
+
+    const statusName = (() => {
+      switch (settings.store.nameFormat) {
+        case NameFormat.ArtistFirst:
+          return trackData.artist + " - " + trackData.name;
+        case NameFormat.SongFirst:
+          return trackData.name + " - " + trackData.artist;
+        case NameFormat.ArtistOnly:
+          return trackData.artist;
+        case NameFormat.SongOnly:
+          return trackData.name;
+        default:
+          return settings.store.statusName;
+      }
+    })();
+
+    return {
+      application_id: applicationId,
+      name: statusName,
+
+      details: trackData.name,
+      state: trackData.artist,
+      assets,
+
+      buttons: buttons.map((v) => v.label),
+      metadata: {
+        button_urls: buttons.map((v) => v.url),
+      },
+
+      type: settings.store.useListeningStatus
+        ? ActivityType.LISTENING
+        : ActivityType.PLAYING,
+      flags: ActivityFlag.INSTANCE,
+    };
+  },
 });
